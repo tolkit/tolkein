66aef26f1e57259378f99b3846944c39fdb2cd14
Richard Challis <richard.challis@sanger.ac.uk>
2021-10-13 10:04:06 +0100

    'Bump version: 0.3.0 → 0.4.0'

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ try:
     version = release = get_distribution("tolkein").version
 except Exception:
     traceback.print_exc()
-    version = release = "0.3.0"
+    version = release = "0.4.0"
 
 pygments_style = "trac"
 templates_path = ["."]
--- src/tolkein/lib/version.py
+++ src/tolkein/lib/version.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 """Tolkein version."""
 
-__version__ = "0.3.0"
+__version__ = "0.4.0"
